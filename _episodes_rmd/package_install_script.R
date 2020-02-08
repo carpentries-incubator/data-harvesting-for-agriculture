ebf215eee130fa3dfe98411039f223e46a82283d
brittanikedge <31941109+brittanikedge@users.noreply.github.com>
2020-02-08 17:54:05 -0600

    Brittani update with packages from lessons
    Added more packages and responded to Jill's comments

--- _episodes_rmd/package_install_script.R
+++ _episodes_rmd/package_install_script.R
@@ -1,12 +1,33 @@
 install.packages("ggplot2")
-install.packages("rgdal")
+install.packages("rgdal") # I'm not sure that we ever use rgdal as we use class sf rather than spdf; I will check this
 install.packages("dplyr") 
-#install.packages("tibble") # need this or no?
+install.packages("sf")
+install.packages("gstat")
+install.packages("tmap")
+install.packages("measurements")
+install.packages("daymetr")
+install.packages("FedData")
+install.packages("lubridate")
+install.packages("raster")
+install.packages("data.table")
+install.packages("broom")
+
+#install.packages("tibble") # need this or no? # I believe this is installed with dplyr so no, but I will check this
 
 # to use
-library(ggplot2)
-library(rgdal)
-library(dplyr)
+library("ggplot2")
+library("rgdal")
+library("dplyr")
+library("sf")
+library("gstat")
+library("tmap")
+library("measurements")
+library("daymetr")
+library("FedData")
+library("lubridate")
+library("raster")
+library("data.table")
+library("broom")
 
 # test ggplot2
 df = data.frame("x"=c(1,2,3), "y"=c(1,2,3))
