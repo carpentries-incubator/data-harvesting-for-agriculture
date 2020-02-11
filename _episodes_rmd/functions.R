d74c7daa5a67695b5ad8e8b9c787ce22ddd76ca9
Brittani Edge <brittaniedge@Brittanis-MacBook-Pro.local>
2020-02-11 15:27:10 -0600

    Commenting out the test lines in the functions script

--- _episodes_rmd/functions.R
+++ _episodes_rmd/functions.R
@@ -5,8 +5,8 @@ long2UTM <- function(long){
   return(utm)
 }
 
-utmzone <- long2UTM(mean(st_bbox(trial)[c(1,3)]))
-utmzone
+# utmzone <- long2UTM(mean(st_bbox(trial)[c(1,3)]))
+# utmzone
 
 as.Date.daymetr <- function(daymetdata){
   date <- as.Date(daymetdata$yday, origin = paste0(daymetdata$year-1, "-12-31"))
@@ -21,12 +21,12 @@ avgprec.by.month <- function(data){
   data %>% dplyr::group_by(month) %>% dplyr::summarise(prec_avg = mean(prec_month)) 
 }
 
-by_month <- by_month_year %>% dplyr::group_by(month) %>% dplyr::summarise(prec_avg = mean(prec_month))
-
-
-sumprec.by.monthyear(weather_data)
-
-group.sum.xy(weather_data, month, year, prec_month, prec)
-
-by_month_year <- weather_data %>% dplyr::group_by(month, year) %>% dplyr::summarise(prec_month = sum(prec))
-
+# by_month <- by_month_year %>% dplyr::group_by(month) %>% dplyr::summarise(prec_avg = mean(prec_month))
+# 
+# 
+# sumprec.by.monthyear(weather_data)
+# 
+# group.sum.xy(weather_data, month, year, prec_month, prec)
+# 
+# by_month_year <- weather_data %>% dplyr::group_by(month, year) %>% dplyr::summarise(prec_month = sum(prec))
+# 
