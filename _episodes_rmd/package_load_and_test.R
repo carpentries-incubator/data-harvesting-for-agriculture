29ad80e0ef0b43f225b6e10fd8b293696ecf54bc
Brittani Edge <brittaniedge@Brittanis-MacBook-Pro.local>
2020-02-10 20:25:20 -0600

    Added tests for each of the packages from vignettes or using data from already installed packages

--- _episodes_rmd/package_load_and_test.R
+++ _episodes_rmd/package_load_and_test.R
@@ -25,3 +25,68 @@ library("ggplot2")
 # test ggplot2
 df = data.frame("x"=c(1,2,3), "y"=c(1,2,3))
 ggplot(df, aes(x=x, y=y)) + geom_point()
+
+# test sf 
+nc <- st_read(system.file("shape/nc.shp", package="sf"))
+plot(nc$geometry) 
+
+# test dplyr
+names(nc)[1]
+nc <- dplyr::rename(nc, area = AREA)
+names(nc)[1]
+
+# test gstat
+library(sp)
+data(meuse)
+coordinates(meuse) = ~x+y
+data(meuse.grid)
+coordinates(meuse.grid) = ~x+y
+gridded(meuse.grid) = TRUE
+
+lzn.vgm = variogram(log(zinc)~1,data = meuse)
+lzn.fit = fit.variogram(lzn.vgm, model = vgm(1, "Sph", 900, 1))
+plot(lzn.vgm, lzn.fit)
+
+# test tmap
+tm_shape(nc) + tm_polygons('area')
+
+# test measurements
+conv_unit(2.54, "cm", "inch")
+
+# test daymetr
+df <- download_daymet(site = "Oak Ridge National Laboratories",
+                      lat = 36.0133,
+                      lon = -84.2625,
+                      start = 2000,
+                      end = 2010,
+                      internal = TRUE,
+                      simplify = TRUE) 
+
+# test FedData
+SSURGO.areas <- get_ssurgo(template=c('CO670'), label='CO_TEST')
+SSURGO.areas.CO670 <- SSURGO.areas$spatial[SSURGO.areas$spatial$AREASYMBOL=='CO670',]
+NED.CO670 <- get_ned(template=SSURGO.areas.CO670, label='SSURGO_CO670')
+plot(NED.CO670)
+
+# test lubridate
+ymd("20110604")
+
+# test raster
+r <- raster(nrows=10, ncols=10)
+r <- setValues(r, 1:ncell(r))
+plot(r)
+
+# test data.table
+DT = data.table(
+  ID = c("b","b","b","a","a","c"),
+  a = 1:6,
+  b = 7:12,
+  c = 13:18
+)
+DT
+
+# test broom
+lmfit <- lm(mpg ~ wt, mtcars)
+tidy(lmfit)
+
+
