ce32840794ecd41fc0dc47ee30ba58e81d94edd2
Brittani Edge <brittaniedge@Brittanis-MacBook-Pro.local>
2020-02-11 16:22:01 -0600

    added more conversion functions and function for center long point and center lat point of an object

--- _episodes_rmd/functions.R
+++ _episodes_rmd/functions.R
@@ -5,6 +5,14 @@ long2UTM <- function(long){
   return(utm)
 }
 
+cent_long <- function(sfobject){
+  mean(st_bbox(sfobject)[c(1,3)])
+}
+
+cent_lat <- function(sfobject){
+  mean(st_bbox(sfobject)[c(2,4)])
+}
+
 # utmzone <- long2UTM(mean(st_bbox(trial)[c(1,3)]))
 # utmzone
 
@@ -20,6 +28,28 @@ sumprec.by.monthyear <- function(data){
 avgprec.by.month <- function(data){
   data %>% dplyr::group_by(month) %>% dplyr::summarise(prec_avg = mean(prec_month)) 
 }
+
+# conversions 
+c_to_f <- function(varc){
+  conv_unit(varc, "C", "F") 
+}
+
+mm_to_in <- function(varc){
+  conv_unit(varc, "mm", "inch") 
+}
+
+cm_to_in <- function(varc){
+  conv_unit(varc, "cm", "inch") 
+}
+
+ha_to_ac <- function(varc){
+  conv_unit(varc, "hectare", "acres") 
+}
+
+kg_to_lb <- function(varc){
+  conv_unit(varc, "kg", "lb") 
+}
+
 
 # by_month <- by_month_year %>% dplyr::group_by(month) %>% dplyr::summarise(prec_avg = mean(prec_month))
 # 
