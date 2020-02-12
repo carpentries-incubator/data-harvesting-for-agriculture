65773b2a8b5125ebc9574c0a3c7c8ccf4b58a975
Brittani Edge <brittaniedge@Brittanis-MacBook-Pro.local>
2020-02-11 22:43:39 -0600

    Making changes to the public data lesson which is now the weather data lesson.

--- _episodes_rmd/functions.R
+++ _episodes_rmd/functions.R
@@ -1,8 +1,15 @@
 ### These are functions found in the Data Carpentry for Agriculture Workshop
 
-long2UTM <- function(long){
+utm_zone <- function(long){
   utm <- (floor((long + 180)/6) %% 60) + 1
   return(utm)
+}
+
+st_transform_utm <- function(sfobject){
+  utmzone <- utm_zone(mean(st_bbox(sfobject)[c(1,3)]))
+  projutm <- as.numeric(paste0("326", utmzone))
+  newobj <- st_transform(sfobject, projutm)
+  return(newobj)
 }
 
 cent_long <- function(sfobject){
@@ -12,9 +19,6 @@ cent_long <- function(sfobject){
 cent_lat <- function(sfobject){
   mean(st_bbox(sfobject)[c(2,4)])
 }
-
-# utmzone <- long2UTM(mean(st_bbox(trial)[c(1,3)]))
-# utmzone
 
 as.Date.daymetr <- function(daymetdata){
   date <- as.Date(daymetdata$yday, origin = paste0(daymetdata$year-1, "-12-31"))
@@ -49,14 +53,3 @@ ha_to_ac <- function(varc){
 kg_to_lb <- function(varc){
   conv_unit(varc, "kg", "lb") 
 }
-
-
-# by_month <- by_month_year %>% dplyr::group_by(month) %>% dplyr::summarise(prec_avg = mean(prec_month))
-# 
-# 
-# sumprec.by.monthyear(weather_data)
-# 
-# group.sum.xy(weather_data, month, year, prec_month, prec)
-# 
-# by_month_year <- weather_data %>% dplyr::group_by(month, year) %>% dplyr::summarise(prec_month = sum(prec))
-# 
