480524c956e79754875ac5dee53cb2c63fb4ca94
Brittani Edge <brittaniedge@Brittanis-MacBook-Pro.local>
2020-02-12 22:40:40 -0600

    Added function for silt, sand, and clay content from ssurgo

--- _episodes_rmd/functions.R
+++ _episodes_rmd/functions.R
@@ -72,3 +72,99 @@ map_poly <- function(sfobject, variable){
     tm_layout(legend.outside = TRUE, frame = FALSE) 
 }
 
+wt_mean <- function(property, weights)
+{
+  # compute thickness weighted mean, but only when we have enough data
+  # in that case return NA
+  
+  # save indices of data that is there
+  property.that.is.na <- which( is.na(property) )
+  property.that.is.not.na <- which( !is.na(property) )
+  
+  if( length(property) - length(property.that.is.na) >= 1)
+    prop.aggregated <- sum(weights[property.that.is.not.na] * property[property.that.is.not.na], na.rm=TRUE) / sum(weights[property.that.is.not.na], na.rm=TRUE)
+  else
+    prop.aggregated <- NA
+  
+  return(prop.aggregated)
+}
+
+profile_total <- function(property, thickness)
+{
+  # compute profile total
+  # in that case return NA
+  
+  # save indices of data that is there
+  property.that.is.na <- which( is.na(property) )
+  property.that.is.not.na <- which( !is.na(property) )
+  
+  if( length(property) - length(property.that.is.na) >= 1)
+    prop.aggregated <- sum(thickness[property.that.is.not.na] * property[property.that.is.not.na], na.rm=TRUE)
+  else
+    prop.aggregated <- NA
+  
+  return(prop.aggregated)
+}
+
+# define a function to perfom hz-thickness weighted aggregtion
+component_level_aggregation <- function(i)
+{
+  # horizon thickness is our weighting vector
+  hz_thick <- i$hzdepb.r - i$hzdept.r
+  
+  # compute wt.mean aggregate values
+  clay <- wt_mean(i$claytotal.r, hz_thick) 
+  silt <- wt_mean(i$silttotal.r, hz_thick)
+  sand <- wt_mean(i$sandtotal.r, hz_thick)
+  # compute profile sum values
+  water_storage <- profile_total(i$awc.r, hz_thick)
+  
+  # make a new dataframe out of the aggregate values
+  d <- data.frame(cokey=unique(i$cokey), clay=clay, silt=silt, sand=sand, water_storage=water_storage)
+  
+  return(d)
+}
+
+mapunit_level_aggregation <- function(i)
+{
+  # component percentage is our weighting vector
+  comppct <- i$comppct.r 
+  
+  # wt. mean by component percent
+  clay <- wt_mean(i$clay, comppct)
+  silt <- wt_mean(i$silt, comppct)
+  sand <- wt_mean(i$sand, comppct)
+  water_storage <- wt_mean(i$water_storage, comppct)
+  
+  # make a new dataframe out of the aggregate values
+  d <- data.frame(mukey=unique(i$mukey), clay=clay, silt=silt, sand=sand, water_storage=water_storage)
+  
+  return(d)
+}
+
+###############################################
+# using these functions on my data
+c_s_s_soil <- function(ssurgo){
+  
+  chorizon<-ssurgo$tabular$chorizon
+  
+  component<-ssurgo$tabular$component 
+  
+  # aggregate horizon data to the component level
+  chorizon.agg <- ddply(chorizon, .(cokey), .fun=component_level_aggregation, .progress='text')
+  
+  # join up the aggregate chorizon data to the component table
+  comp.merged <- merge(component, chorizon.agg, by='cokey')
+  
+  # aggregate component data to the map unit level
+  component.agg <- ddply(comp.merged, .(mukey), .fun=mapunit_level_aggregation, .progress='text')
+  
+  # save data to the ssurgo shapefile
+  spatial <- as(ssurgo$spatial, "sf")
+  spatial <- rename(spatial, c("MUKEY" = "mukey"))
+  spatial <- merge.data.frame(spatial, component.agg, by="mukey")
+  spatial <- spatial[,-5]
+  spatial <- dplyr::distinct(spatial)
+  # spatial <- spatial[,6:9]
+  return(spatial)
+}
