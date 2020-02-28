c2943cbada4f42828a363ee4bcd8ddaf05454527
Jill Naiman <jnaiman@gmail.com>
2020-02-27 20:37:10 -0600

    about to clean the data sim script

--- _episodes_rmd/simulate_random_plot.R
+++ _episodes_rmd/simulate_random_plot.R
@@ -43,7 +43,15 @@ abline_utm = st_transform_utm(abline)
 # trial grid
 trial <- read_sf("/Users/jillnaiman/trial-lesson_ag/_episodes_rmd/data/trial.gpkg")
 ##trial <- st_read("/Users/jillnaiman/Dropbox/agriculture_SC_workshop_Feb2020/shifted_gpkg/trial.gpkg") # read in boundary
-trial_utm <- trial # note: DO NOT TRY TO CONVERT
+trial_utm <- trial # note: DO NOT TRY TO CONVERT TO UTM
+# make sure we only get in "trial" part of the grid -> from the old boundary file
+#boundary_trial = subset(boundary_old_utm, boundary_old_utm$Type == 'Trial')
+trial_grid <- trial_utm # was subsetting with "Trial" but not anymore
+#trial_grid <- st_intersection(trial_utm, boundary_trial)
+# check out this boundary
+#par(mfrow=c(1,1))
+#tm_shape(trial_grid) + tm_borders(col='blue')
+
 
 # Yield data - what was actually plotted
 yield <- read_sf("/Users/jillnaiman/trial-lesson_ag/_episodes_rmd/data/yield.gpkg")
@@ -51,87 +59,111 @@ yield_utm <- st_transform_utm(yield)
 # if you wanna check it out
 #hist(yield_utm$Yld_Vol_Dr)
 
-#  now cleaning
+#  now cleaning for yields
 yield_clean_border <- clean_buffer(trial_utm, 15, yield_utm) # clean border
 yield_clean <- clean_sd(yield_clean_border, yield_clean_border$Yld_Vol_Dr) # clean by 3SD
 hist(yield_clean$Yld_Vol_Dr) # if you wanna check the cleaned data
-# write cleaned
-st_write(yield_clean, "/Users/jillnaiman/Dropbox/agriculture_SC_workshop_Feb2020/cleaned_gpkg/yield_clean.gpkg", layer_options = 'OVERWRITE=YES')
 
-subplots_data <- deposit_on_grid(trial_utm, yield_clean, "Yld_Vol_Dr", fn = median)
-map_poly(subplots_data_yield, 'Yld_Vol_Dr', "Orig Yield")
+# deposit yields on grid - this are plants that actually grew
+subplots_data <- deposit_on_grid(trial_grid, yield_clean, "Yld_Vol_Dr", fn = median) # deposit yields
+map_poly(subplots_data, 'Yld_Vol_Dr', "Orig Yield") # plot
 
-
+# these are seeds as planted
 asplanted <- st_read("/Users/jillnaiman/trial-lesson_ag/_episodes_rmd/data/asplanted.gpkg")
 asplanted_utm <- st_transform_utm(asplanted)
 plot(asplanted_utm$geom)
 asplanted_clean_border <- clean_buffer(trial_utm, 15, asplanted_utm)
 plot(asplanted_clean_border$geom)
 asplanted_clean <- clean_sd(asplanted_clean_border, asplanted_clean_border$Rt_Apd_Ct_)
-#par(mfrow=c(1,2))
-asplanted_plot <- map_points(asplanted_clean, "Rt_Apd_Ct_", "Seed")
-trial_plot <- map_poly(subplots_data_yield, 'Yld_Vol_Dr', "Orig Yield")
-boundary_plot <- map_poly(boundary_old, 'Type', "Type")
-plot_comp <- tmap_arrange(asplanted_plot, trial_plot, boundary_plot, ncol = 3, nrow = 1)
-plot_comp
+
+# maken some plots:
+#asplanted_plot <- map_points(asplanted_clean, "Rt_Apd_Ct_", "Seed")
+#trial_plot <- map_poly(subplots_data, 'Yld_Vol_Dr', "Orig Yield")
+#boundary_plot <- map_poly(boundary_old, 'Type', "Type")
+#plot_comp <- tmap_arrange(asplanted_plot, trial_plot, boundary_plot, ncol = 3, nrow = 1)
+#plot_comp
+
+# deposit the elevation and rate applied as elevation & rt applied
+subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Rt_Apd_Ct_", fn = median) # deposit planted seeds
+subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Elevation_", fn = median) # deposit elevation
+# this is the designed rate too:
+map_seed = map_poly(trial_utm, "SEEDRATE", "SEEDRATE")
+#subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Elevation_", fn = median)
+
+# aggregated asplanted grid plot
+#asplanted_plot_grid <- map_poly(subplots_data, "Rt_Apd_Ct_", "Rt_Apd_Ct_")
+#asplanted_plot_grid
+#plot_comp2 <- tmap_arrange(asplanted_plot_grid, map_seed, ncol = 2, nrow = 1)
+#plot_comp2
 
 #map_points(asplanted_clean, "Rt_Apd_Ct_", "Seed")
 #plot(trial_utm$geom)
 
-hist(asplanted_clean$Rt_Apd_Ct_)
+# plot cleaned value
+#hist(asplanted_clean$Rt_Apd_Ct_)
 
-subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Rt_Apd_Ct_", fn = median)
-#subplots_data2 <- deposit_on_grid(trial_utm, asplanted_clean, "Rt_Apd_Ct_", fn = median)
+# asplanted vs. yield
+#par(mfrow=c(1,1))
+#plot(subplots_data$Rt_Apd_Ct_, subplots_data$Yld_Vol_Dr)
 
-subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Elevation_", fn = median)
 
 nitrogen <- read_sf("/Users/jillnaiman/trial-lesson_ag/_episodes_rmd/data/asapplied.gpkg")
 nitrogen_utm = st_transform_utm(nitrogen)
-nitrogen_clean_border <- clean_buffer(trial_utm, 1, nitrogen_utm)
-nitrogen_clean <- clean_sd(nitrogen_clean_border, nitrogen_clean_border$Rate_Appli)
+#nitrogen_clean_border <- clean_buffer(trial_utm, 1, nitrogen_utm) # can't really clean, or so they say
+#nitrogen_clean <- clean_sd(nitrogen_clean_border, nitrogen_clean_border$Rate_Appli)
+nitrogen_clean <- clean_sd(nitrogen_utm, nitrogen_utm$Rate_Appli)
 hist(nitrogen_clean$Rate_Appli)
 subplots_data <- deposit_on_grid(subplots_data, nitrogen_clean, "Rate_Appli", fn = median)
 
 # Plot fit model
 par(mfrow=c(1,3))
 # do simple MLR
-myMod = lm(Yld_Vol_Dr~Rate_Appli+Rt_Apd_Ct_+Elevation_, data=subplots_data)
+#myMod = lm(Yld_Vol_Dr~Rate_Appli+Rt_Apd_Ct_+Elevation_, data=subplots_data)
+myMod = glm(Yld_Vol_Dr~Rate_Appli+Rt_Apd_Ct_+Elevation_, data=subplots_data)
 nPoints = 1000
 rapp = seq(130, 200, length=nPoints)
-rseed = seq(29000, 39000, length=nPoints)
+#rapp_rates = c(130, 160,200,225,250) # only certain rates
+#rapp = sample(rapp_rates, size=nPoints, replace=TRUE)
+rseed_rates = c(30000, 32000, 34000, 36000, 38000) # only certain rates
+rseed = sample(rseed_rates, size=nPoints, replace=TRUE)
 elev = seq(1000, 1050, length=nPoints)
 
 # Grab SE about each coefficient
-yieldsSTD = coef(summary(myMod))[, "Std. Error"]
+#yieldsSTD = coef(summary(myMod))[, "Std. Error"]/length(subplots_data$Rate_Appli)**0.5
+# # generate new yields points from these fits, but with randomly chosen coefficients
+# coeff1 = rnorm(nPoints, mean=myMod$coefficients[1], sd = yieldsSTD[1])
+# #coeff1 = myMod$coefficients[1]
+# coeff2 = rnorm(nPoints, mean=myMod$coefficients[2], sd = yieldsSTD[2])
+# coeff3 = rnorm(nPoints, mean=myMod$coefficients[3], sd = yieldsSTD[3])
+# coeff4 = rnorm(nPoints, mean=myMod$coefficients[4], sd = yieldsSTD[4])
+# # create new model:
+# yieldsMod = coeff1 + coeff2*rapp + coeff3*rseed + coeff4*elev
+# 
+# yieldsModLine = myMod$coefficients[1] + myMod$coefficients[2]*rapp + myMod$coefficients[3]*rseed + myMod$coefficients[4]*elev
 
-# generate new yields points from these fits, but with randomly chosen coefficients
-#coeff1 = rnorm(nPoints, mean=myMod$coefficients[1], sd = yieldsSTD[1])
-coeff1 = myMod$coefficients[1]
-coeff2 = rnorm(nPoints, mean=myMod$coefficients[2], sd = yieldsSTD[2])
-coeff3 = rnorm(nPoints, mean=myMod$coefficients[3], sd = yieldsSTD[3])
-coeff4 = rnorm(nPoints, mean=myMod$coefficients[4], sd = yieldsSTD[4])
-# create new model:
-yieldsMod = coeff1 + coeff2*rapp + coeff3*rseed + coeff4*elev
+#library(MASS) # For multivariate normal distribution, handy later on => don't wanna use MASS library!
+#coefs <- mvrnorm(n = nPoints, mu = coefficients(myMod), Sigma = vcov(myMod))
+coefs <- rnorm(n = nPoints, mean = coefficients(myMod), sd = vcov(myMod))
 
-yieldsModLine = myMod$coefficients[1] + myMod$coefficients[2]*rapp + myMod$coefficients[3]*rseed + myMod$coefficients[4]*elev
+yieldsMod = coefs[,'(Intercept)'] + coefs[, 'Rate_Appli']*rapp + coefs[, 'Rt_Apd_Ct_']*rseed + coefs[, 'Elevation_']*elev
 
 # PLOT:
 # yield, nitrogen
 #y=Yld_Vol_Dr,x=Rate_Appli
-plot(subplots_data$Rate_Appli, subplots_data$Yld_Vol_Dr, ylim=c(100, 300))
-lines(rapp, yieldsModLine, col="red")
-#points(rapp, yieldsMod, col='blue')
+plot(subplots_data$Rate_Appli, subplots_data$Yld_Vol_Dr, ylim=c(100, 300))#, xlim=c(130, 250))
+#lines(rapp, yieldsModLine, col="red")
+points(rapp, yieldsMod, col='blue')
 
 # seeding
 #y=Yld_Vol_Dr,x=Rt_Apd_Ct_
 plot(subplots_data$Rt_Apd_Ct_, subplots_data$Yld_Vol_Dr, ylim=c(100, 300))
-lines(rseed, yieldsModLine, col="red")
-#points(rseed, yieldsMod, col='blue')
+#lines(rseed, yieldsModLine, col="red")
+points(rseed, yieldsMod, col='blue')
 
 # elevation
 #y=Yld_Vol_Dr,x=Elevation_
 plot(subplots_data$Elevation_, subplots_data$Yld_Vol_Dr, ylim=c(100, 300))
-lines(elev, yieldsModLine, col="red")
-#points(elev, yieldsMod, col='blue')
+#lines(elev, yieldsModLine, col="red")
+points(elev, yieldsMod, col='blue')
 
 
