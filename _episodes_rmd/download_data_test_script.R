d87c0b39af2cafada300b82741f7c27fb62085d1
Dena <dlstrong@illinois.edu>
2020-03-04 14:05:48 -0600

    Updating comments and changing the default working directory example

--- _episodes_rmd/download_data_test_script.R
+++ _episodes_rmd/download_data_test_script.R
@@ -5,15 +5,23 @@
 # Step 4: test setup
 
 # STEP 1: setwd
-#setwd('/Users/jillnaiman/testwd/')  #### UPDATE HERE
+#setwd("C:/DataHarvestingWin/WorkingDir")  #### UPDATE HERE
 
 # STEP 2: source functions.R from web 
 source('https://raw.githubusercontent.com/data-carpentry-for-agriculture/trial-lesson/gh-pages/_episodes_rmd/functions.R')
 
-# STEP 3: re-download data, I don't *THINK* we will need this... I am lying we totally do.
+# STEP 3: re-download data
 download_workshop_data() # download all the data
 
 # STEP 4: Run test script and see about outputs
 run_workshop_test()
 
-
+# When you've run this script, look for:
+# [1] "Three text elements:"
+# [1] "area"
+# [1] 1
+# [1] "2011-06-04"
+# in your console window. 
+#
+# Then in File Explorer, inside your working directory, look for a folder called test_images
+# containing 9 images of data.
