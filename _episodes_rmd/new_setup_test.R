d957d5830e1662a6cb1d181b221f0b3450026096
Jill Naiman <jnaiman@gmail.com>
2020-03-04 12:45:19 -0600

    stuff for test script

--- _episodes_rmd/new_setup_test.R
+++ _episodes_rmd/new_setup_test.R
@@ -71,3 +71,99 @@ if (dir.exists(paste0(getwd(),"/data"))){
     }    
   }  
 }
+
+# STEP 4: "test my setup"
+
+# copied from before:
+library("rgdal")
+library("plyr")
+library("dplyr")
+library("sp")
+library("sf")
+library("gstat")
+library("tmap")
+library("measurements")
+library("daymetr")
+library("FedData")
+library("lubridate")
+library("raster")
+library("data.table")
+library("broom")
+library("ggplot2")
+
+#set margins
+par(mar=c(1,1,1,1))
+
+# plot #1
+# test ggplot2
+df = data.frame("x"=c(1,2,3), "y"=c(1,2,3))
+ggplot(df, aes(x=x, y=y)) + geom_point()
+
+# plot #2
+# test sf 
+nc <- st_read(system.file("shape/nc.shp", package="sf"))
+plot(nc$geometry) 
+
+# text #1
+# test dplyr
+names(nc)[1]
+nc <- dplyr::rename(nc, area = AREA)
+names(nc)[1]
+
+# plot #3
+# test gstat
+library(sp)
+data(meuse)
+coordinates(meuse) = ~x+y
+data(meuse.grid)
+coordinates(meuse.grid) = ~x+y
+gridded(meuse.grid) = TRUE
+#   plot
+lzn.vgm = variogram(log(zinc)~1,data = meuse)
+lzn.fit = fit.variogram(lzn.vgm, model = vgm(1, "Sph", 900, 1))
+plot(lzn.vgm, lzn.fit)
+
+# plot #4
+# test tmap
+tm_shape(nc) + tm_polygons('area')
+
+# test measurements
+conv_unit(2.54, "cm", "inch")
+
+# test daymetr
+df <- download_daymet(site = "Oak Ridge National Laboratories",
+                      lat = 36.0133,
+                      lon = -84.2625,
+                      start = 2000,
+                      end = 2010,
+                      internal = TRUE,
+                      simplify = TRUE) 
+
+# test FedData
+ssurgo <- get_ssurgo(template=c('NC019'), label='county')
+plotssurgo <- ssurgo$spatial
+plotssurgo <- plotssurgo[4000,]
+plot(plotssurgo)
+
+# test lubridate
+ymd("20110604")
+
+# test raster
+r <- raster(nrows=10, ncols=10)
+r <- setValues(r, 1:ncell(r))
+plot(r)
+
+# test data.table
+DT = data.table(
+  ID = c("b","b","b","a","a","c"),
+  a = 1:6,
+  b = 7:12,
+  c = 13:18
+)
+DT
+
+# test broom
+lmfit <- lm(mpg ~ wt, mtcars)
+tidy(lmfit)
+
+
