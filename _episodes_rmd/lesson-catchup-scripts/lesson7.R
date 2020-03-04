59acde87f3abfc58cba2444c3e257cebbe08eada
Dena Strong <dlstrong@illinois.edu>
2020-03-04 16:22:11 -0600

    Matching script to current lesson 7

--- _episodes_rmd/lesson-catchup-scripts/lesson7.R
+++ _episodes_rmd/lesson-catchup-scripts/lesson7.R
@@ -1,7 +1,5 @@
 # Lesson 7 catch-up file - current as of March 4, 2020
 
-# Lesson 8 catch-up file - current as of March 4, 2020
-
 # Find out whether you're working in the directory you intend:
 getwd()
 
@@ -19,69 +17,147 @@ setwd("C:/DataHarvestingWin/WorkingDir")
 # the Source button.)
 source('C:/DataHarvestingWin/WorkingDir/package_load_and_test.R')
 
+# Loading boundary file
+boundary <- read_sf("data/boundary.gpkg")
 
-
-
-
-########
-
-# Starting to create a trial design - load files
-boundary <- st_read("data/boundary.gpkg") # read in boundary
-abline <- st_read("data/abline.gpkg") # read in AB line
-
-# Check coordinate reference system
+# Checking CRS
 st_crs(boundary)
-st_crs(abline)
 
-# Transform them from lat/long to UTM
+# Transforming to UTM
 boundary_utm <- st_transform_utm(boundary)
-abline_utm <- st_transform_utm(abline)
+st_crs(boundary_utm)
 
-# remove headlands
-trialarea <- subset(boundary_utm, Type == "Trial")
+# Getting the trial design too
+trial <- read_sf("data/trial.gpkg")
 
-# define our parameters
-width_in_meters = 24 # width of grids is 24 meters
-long_direction = 'NS' # direction of grid that will be long
-short_direction = 'EW' # direction of grid that will be short
-length_in_ft = 180 # length of grids in feet
+# Looking at trial CRS
+st_crs(trial)
 
-# make grids
-width <- m_to_ft(24) # convert meters to feet
-design_grids_utm <- make_grids(trialarea, abline_utm,
-                               long_in = long_direction,
-                               short_in = short_direction,
-                               length_ft = length_in_ft,
-                               width_ft = width)
+# Transforming to UTM
+trial_utm <- trial
 
-# overlay grids on trial area
-st_crs(design_grids_utm) <- st_crs(trialarea)
-trial_grid <- st_intersection(trialarea, design_grids_utm)
+# EXERCISE: Same with yield data
+yield <- read_sf("data/yield.gpkg")
+st_crs(yield)
+yield_utm <- st_transform_utm(yield)
 
-# look at the plots
-tm_shape(trial_grid) + tm_borders(col='blue')
+# Same with abline file
+abline = st_read("data/abline.gpkg")
+st_crs(abline)
+abline_utm = st_transform_utm(abline)
 
-# determine subplot treatments
-seed_rates <- c(31000, 34000, 37000, 40000)
-nitrogen_rates <- c(160,200,225,250)
+# Clean the yield data
+# First look at what seems odd
+hist(yield_utm$Yld_Vol_Dr)
 
-# here's what goes in the headlands
-seed_quo <- 37000
-nitrogen_quo <- 225
+# Trim the borders because those are often 
+# differently seeded and fertilized than the rest
+yield_clean_border <- clean_buffer(trial_utm, 15, yield_utm)
 
-# generate treatment map
-whole_plot <- treat_assign(trialarea, trial_grid, head_buffer_ft = width,
-                           seed_treat_rates = seed_rates,
-                           nitrogen_treat_rates = nitrogen_rates,
-                           seed_quo = seed_quo,
-                           nitrogen_quo = nitrogen_quo)
+# Compare original and cleaned versions
+yield_plot_orig <- map_points(yield_utm, "Yld_Vol_Dr", "Yield, Orig")
+yield_plot_border_cleaned <- map_points(yield_clean_border, "Yld_Vol_Dr", "Yield, No Borders")
+yield_plot_comp <- tmap_arrange(yield_plot_orig, yield_plot_border_cleaned, ncol = 2, nrow = 1)
+yield_plot_comp
 
-# check it out
-head(whole_plot)
+# Look at the cleaned histogram
+hist(yield_clean_border$Yld_Vol_Dr)
 
-# show side by side comparisons
-nitrogen_plot <- map_poly(whole_plot, "NRATE", "Nitrogen Treatment")
-seed_plot <- map_poly(whole_plot, "SEEDRATE", "Seedrate Treatment")
-treatment_plot_comp <- tmap_arrange(nitrogen_plot, seed_plot, ncol = 2, nrow = 1)
-treatment_plot_comp
+# Some things are still odd, so let's clean again
+yield_clean <- clean_sd(yield_clean_border, yield_clean_border$Yld_Vol_Dr)
 
+# Let's look at the results of the second cleaning
+hist(yield_clean$Yld_Vol_Dr)
+
+# Look at the cleaned up yield map
+yield_plot_clean <- map_points(yield_clean, "Yld_Vol_Dr", "Yield, Cleaned")
+yield_plot_clean
+
+# EXERCISE: Cleaning up the nitrogen data from asapplied.gpkg
+nitrogen <- read_sf("data/asapplied.gpkg")
+st_crs(nitrogen)
+nitrogen_utm <- nitrogen
+nitrogen_clean_border <- clean_buffer(trial_utm, 1, nitrogen_utm)
+nitrogen_plot_orig <- map_points(nitrogen_utm, "Rate_Appli", "Nitrogen, Orig")
+nitrogen_plot_border_cleaned <- map_points(nitrogen_clean_border, "Rate_Appli", "Nitrogen, No Borders")
+nitrogen_plot_comp <- tmap_arrange(nitrogen_plot_orig, nitrogen_plot_border_cleaned, ncol = 2, nrow = 1)
+nitrogen_plot_comp
+nitrogen_clean <- clean_sd(nitrogen_clean_border, nitrogen_clean_border$Rate_Appli)
+nitrogen_plot_clean <- map_points(nitrogen_clean, "Rate_Appli", "Nitrogen, Cleaned")
+nitrogen_plot_clean
+hist(nitrogen_clean$Rate_Appli)
+
+# Designing trials: Making grids
+width = m_to_ft(24) # convert from meters to feet
+design_grids_utm = make_grids(boundary_utm,
+                              abline_utm, long_in = 'NS', short_in = 'EW',
+                              length_ft = width, width_ft = width)
+st_crs(design_grids_utm)
+st_crs(design_grids_utm) = st_crs(boundary_utm)
+
+# Show the grid
+plot(design_grids_utm$st_sfc.col_polygons_ls.)
+
+# Lay the grid onto our field's boundary information
+trial_grid_utm = st_intersection(boundary_utm, design_grids_utm)
+
+# Show the field with the grid
+plot(trial_grid_utm$geom)
+
+# Place the cleaned yield data into the grid
+subplots_data <- deposit_on_grid(trial_grid_utm, yield_clean, "Yld_Vol_Dr", fn = median)
+
+# Display the results
+map_poly(subplots_data, 'Yld_Vol_Dr', "Yield (bu/ac)")
+
+# Next up: Doing the same for as-planted data
+asplanted <- st_read("data/asplanted_new.gpkg")
+st_crs(asplanted)
+asplanted_utm <- asplanted # already in utm!
+asplanted_clean <- clean_sd(asplanted_utm, asplanted_utm$Rt_Apd_Ct_)
+asplanted_clean <- clean_buffer(trial_utm, 15, asplanted_clean)
+
+# Show the results
+map_points(asplanted_clean, "Rt_Apd_Ct_", "Seed")
+
+# Using our grid with the as-planted data
+subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Rt_Apd_Ct_", fn = median)
+subplots_data <- deposit_on_grid(subplots_data, asplanted_clean, "Elevation_", fn = median)
+
+# Show the results
+map_poly(subplots_data, 'Rt_Apd_Ct_', "Seed")
+
+# Using our grid with the nitrogen as applied data
+subplots_data <- deposit_on_grid(subplots_data, nitrogen_clean, "Rate_Appli", fn = median)
+
+# Show the results
+map_poly(subplots_data, 'Rate_Appli', "Nitrogen")
+
+# Showing relationships between variables
+# Yield and elevation:
+ggplot() +
+  geom_smooth(data = subplots_data, method = "gam", aes(y=Yld_Vol_Dr,x=Elevation_), size = 0.5, se=FALSE) +
+  ylab('Yield (kg/ha)') +
+  xlab('Elevation') + 
+  theme_grey(base_size = 12)
+
+# Yield and seed rates:
+ggplot() +
+  geom_smooth(data = subplots_data, method = "gam", aes(y=Yld_Vol_Dr,x=Rt_Apd_Ct_), size = 0.5, se=FALSE) +
+  ylab('Yield (kg/ha)') +
+  xlab('Seed (k/ha)') + 
+  theme_grey(base_size = 12)
+
+# Yield and nitrogen:
+ggplot() +
+  geom_smooth(data = subplots_data, method = "gam", aes(y=Yld_Vol_Dr,x=Rate_Appli), size = 0.5, se=FALSE) +
+  ylab('Yield (kg/ha)') +
+  xlab('Nitrogen') + 
+  theme_grey(base_size = 12)
+
+# Comparing what seed rate was intended with
+# the seed rate that was applied, to see how accurate
+# the variable rate application was
+
+subplots_data <- deposit_on_grid(subplots_data, trial_utm, "SEEDRATE", fn = median)
+map_poly(subplots_data, 'SEEDRATE', "Target Seed")
